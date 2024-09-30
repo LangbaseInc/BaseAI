6def2d90a01c66b49568c97ac0002aa03dbd483a
Ahmad Awais <me@AhmadAwais.com>
2024-09-30 00:03:51 -0700

    👌 IMPROVE: Tile

--- apps/baseai.dev/src/app/layout.tsx
+++ apps/baseai.dev/src/app/layout.tsx
@@ -6,19 +6,19 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: {
-		template: 'BaseAI - The first AI framework for web',
-		default: 'BaseAI'
+		template: 'BaseAI - Web AI Framework',
+		default: 'BaseAI - Web AI Framework'
 	},
 	description: `BaseAI helps developers locally build AI agents, memory (RAG), and then deploy them to a highly scalable API. It is the first AI framework for Web. It's composable by design and offers a simple API to build and deploy any AI agents (AI features).`,
 	openGraph: {
-		title: 'BaseAI - The first AI framework for web',
+		title: 'BaseAI - Web AI Framework',
 		description: `BaseAI helps developers locally build AI agents, memory (RAG), and then deploy them to a highly scalable API. It is the first AI framework for Web. It's composable by design and offers a simple API to build and deploy any AI agents (AI features).`,
 		images: ['https://baseai.dev/api/og?title=baseai.dev'],
 		siteName: 'BaseAI'
 	},
 	twitter: {
 		card: 'summary_large_image',
-		title: 'BaseAI - The first AI framework for web',
+		title: 'BaseAI - Web AI Framework',
 		creator: '@LangbaseInc',
 		description: `BaseAI helps developers locally build AI agents, memory (RAG), and then deploy them to a highly scalable API. It is the first AI framework for Web. It's composable by design and offers a simple API to build and deploy any AI agents (AI features).`,
 		images: ['https://baseai.dev/api/og?title=baseai.dev']
