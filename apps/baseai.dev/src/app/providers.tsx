704dba24d4e8f0b66d3c1bfd1825ba5d6cd72abc
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-09-30 20:33:39 +0500

    📦 NEW: Force dark mode on home page

--- apps/baseai.dev/src/app/providers.tsx
+++ apps/baseai.dev/src/app/providers.tsx
@@ -3,6 +3,9 @@
 import { useEffect } from 'react';
 import { ThemeProvider, useTheme } from 'next-themes';
 import { RecoilRoot } from 'recoil';
+import { usePathname } from 'next/navigation';
+
+const FORCE_DARK_MODE_PAGES = ['/'];
 
 function ThemeWatcher() {
 	let { resolvedTheme, setTheme } = useTheme();
@@ -29,8 +32,14 @@ function ThemeWatcher() {
 }
 
 export function Providers({ children }: { children: React.ReactNode }) {
+	const pathname = usePathname();
+	const website = FORCE_DARK_MODE_PAGES.includes(pathname);
 	return (
-		<ThemeProvider attribute="class" disableTransitionOnChange>
+		<ThemeProvider
+			attribute="class"
+			disableTransitionOnChange
+			forcedTheme={website ? 'dark' : undefined}
+		>
 			<ThemeWatcher />
 			<RecoilRoot>{children}</RecoilRoot>
 		</ThemeProvider>
