1d4004172bb53a1548e20036c096a7e249b0d288
Ahmad Awais <me@AhmadAwais.com>
2024-09-29 18:10:45 -0700

    🐛 FIX: testing

---
+++ packages/testing/index.ts
@@ -0,0 +1,20 @@
+// import {Pipe} from '@baseai/core';
+// import pipeSummary from '../baseai/pipes/summary';
+
+// const pipe = new Pipe(pipeSummary());
+
+// async function main() {
+// 	const userMsg = 'Who is an AI Engineer?';
+
+// 	const response = await pipe.run({
+// 		messages: [
+// 			{
+// 				role: 'user',
+// 				content: userMsg,
+// 			},
+// 		],
+// 	});
+// 	console.log('response: ', response);
+// }
+
+// main();
