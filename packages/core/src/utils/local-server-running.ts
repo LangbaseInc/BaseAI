bf5bd9695b8b9c90f3dd4d0ddae3bb9ae1dfced7
msaaddev <mrsaadirfan@gmail.com>
2024-10-02 23:17:06 +0200

    👌 IMPROVE: Error message

--- packages/core/src/utils/local-server-running.ts
+++ packages/core/src/utils/local-server-running.ts
@@ -9,7 +9,7 @@ export async function isLocalServerRunning(): Promise<Boolean> {
 			cache: 'no-cache', // Prevents caching of the request
 		});
 
-		const portUseError = `\nPort 9000 is already in use. \nTerminate the process running on it. \nRun npx baseai@latest dev in an new terminal to start the dev server.\n`;
+		const portUseError = `\nPort 9000 is already in use. \nTerminate the process running on it. \nRun "npx baseai@latest dev" in an new terminal to start the dev server.\n`;
 
 		if (!response.ok) {
 			console.error(portUseError);
@@ -29,7 +29,7 @@ export async function isLocalServerRunning(): Promise<Boolean> {
 	} catch (error) {
 		// Port is not in use and BaseAI dev server is not running
 		console.error(
-			`\nBaseAI dev server is not running. \nPlease run npx baseai@latest dev in a new teriminal to start dev server.\n`,
+			`\nBaseAI dev server is not running. \nPlease run "npx baseai@latest dev" in a new teriminal to start dev server.\n`,
 		);
 		return false;
 	}
