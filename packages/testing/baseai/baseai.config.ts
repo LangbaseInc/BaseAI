3e3dfea98167b3f7498d85ac67012de04ed99506
Ahmad Awais <me@AhmadAwais.com>
2024-10-09 01:08:36 -0700

    📦 NEW: Config based env

---
+++ packages/testing/baseai/baseai.config.ts
@@ -0,0 +1,24 @@
+export const config = {
+	log: {
+		isEnabled: true,
+		isEnabledInProd: true,
+		logSensitiveData: false,
+		pipe: true,
+		'pipe.completion': true,
+		'pipe.request': true,
+		'pipe.response': true,
+		'pipe.request.prodOptions': false,
+		'pipe.request.localOptions': false,
+		tool: false,
+		memory: false,
+	},
+	memory: {
+		useLocalEmbeddings: false,
+	},
+	envFilePath: '.env',
+	env: {
+		NODE_ENV: 'production',
+		LANGBASE_API_KEY: process.env.LANGBASE_API_KEY,
+		OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+	},
+};
