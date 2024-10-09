3a9f5f4c4880e5062a6ac4dbe31329fe0ed188b8
Saqib Ameen <saqib1@ualberta.ca>
2024-10-09 10:31:17 -0600

    👌 IMPROVE: Update examples

--- examples/nodejs/baseai/memory/chat-with-docs/index.ts
+++ examples/nodejs/baseai/memory/chat-with-docs/index.ts
@@ -1,8 +1,14 @@
 import {MemoryI} from '@baseai/core';
+import path from 'path';
 
 const buildMemory = (): MemoryI => ({
 	name: 'chat-with-docs',
 	description: 'Chat with docs',
+	config: {
+		useGitRepo: false,
+		dirToTrack: path.posix.join(''),
+		extToTrack: ['*'],
+	},
 });
 
 export default buildMemory;
--- examples/remix/baseai/memory/chat-with-docs/index.ts
+++ examples/remix/baseai/memory/chat-with-docs/index.ts
@@ -1,8 +1,14 @@
-import { MemoryI } from '@baseai/core';
+import {MemoryI} from '@baseai/core';
+import path from 'path';
 
 const buidMemory = (): MemoryI => ({
 	name: 'chat-with-docs',
 	description: 'Chat with given docs',
+	config: {
+		useGitRepo: false,
+		dirToTrack: path.posix.join(''),
+		extToTrack: ['*'],
+	},
 });
 
 export default buidMemory;
