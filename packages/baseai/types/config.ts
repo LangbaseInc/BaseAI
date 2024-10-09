17c0dbdc4af4d2ceb77a48233f14a1ca9e188d31
Ahmad Awais <me@AhmadAwais.com>
2024-10-09 08:14:52 -0700

    📦 NEW: config env optional

--- packages/baseai/types/config.ts
+++ packages/baseai/types/config.ts
@@ -29,16 +29,17 @@ export interface BaseAIConfig {
 	log: LoggerConfig;
 	memory: MemoryConfig;
 	envFilePath: string;
-	env: {
-		NODE_ENV: string;
-		OPENAI_API_KEY: string;
-		ANTHROPIC_API_KEY: string;
-		COHERE_API_KEY: string;
-		FIREWORKS_API_KEY: string;
-		GOOGLE_API_KEY: string;
-		GROQ_API_KEY: string;
-		MISTRAL_API_KEY: string;
-		PERPLEXITY_API_KEY: string;
-		TOGETHER_API_KEY: string;
+	env?: {
+		NODE_ENV?: string;
+		LANGBASE_API_KEY?: string;
+		OPENAI_API_KEY?: string;
+		ANTHROPIC_API_KEY?: string;
+		COHERE_API_KEY?: string;
+		FIREWORKS_API_KEY?: string;
+		GOOGLE_API_KEY?: string;
+		GROQ_API_KEY?: string;
+		MISTRAL_API_KEY?: string;
+		PERPLEXITY_API_KEY?: string;
+		TOGETHER_API_KEY?: string;
 	};
 }
