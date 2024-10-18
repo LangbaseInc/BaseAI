c1810f271c664a72b7133702d11304fb0c9d9a27
msaaddev <mrsaadirfan@gmail.com>
2024-10-19 00:53:01 +0200

    👌 IMPROVE: Header

--- apps/baseai.dev/src/components/Header.tsx
+++ apps/baseai.dev/src/components/Header.tsx
@@ -80,10 +80,10 @@ function HeaderLinks() {
 			<Anchor
 				href={'https://github.com/LangbaseInc/baseai'}
 				target="_blank"
-				className="hidden w-auto font-normal outline outline-[1.5px] outline-black/35 lg:flex lg:items-center dark:outline-white/35"
-				variant={'ghost'}
+				className="hidden xl:flex group border-primary/50"
+				variant={'outline'}
 			>
-				<GitHubIcon className="size-4 text-black dark:text-white" />
+				<GitHubIcon className="size-4 text-black dark:text-white group-hover:text-white group-hover:dark:text-black" />
 				<span className="leading-none flex-grow truncate">Star us on GitHub</span>
 			</Anchor>
 		</>
