801dc257bf10b209e31b06a94259f41c301d8aff
msaaddev <mrsaadirfan@gmail.com>
2024-10-27 02:36:35 +0200

    👌 IMPROVE: Code

--- apps/baseai.dev/src/components/home/hero.tsx
+++ apps/baseai.dev/src/components/home/hero.tsx
@@ -12,8 +12,8 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Hero({ }) {
 	return (
-		<div className="flex align-center min-h-screen flex-col items-center justify-center relative">
-			<div className="xmin-h-screen h-full webgl absolute -top-[13vh] flex w-full sm:top-0">
+		<div className="flex align-center min-h-[90vh] flex-col items-center justify-center relative">
+			<div className="xmin-h-screen h-full webgl absolute -top-[20vh] flex w-full sm:top-0">
 				<WebGLInitializer />
 			</div>
 			<Content />
