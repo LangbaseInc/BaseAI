3c677d34e6b0d401b008258da6dcc58c842aad9a
Ashar Irfan <mrasharirfan@gmail.com>
2024-11-01 04:47:58 +0500

    📦 NEW: Get provider function

---
+++ packages/baseai/src/dev/utils/get-provider.ts
@@ -0,0 +1,50 @@
+import {
+	ANTHROPIC,
+	COHERE,
+	FIREWORKS_AI,
+	GOOGLE,
+	GROQ,
+	OLLAMA,
+	OPEN_AI,
+	PERPLEXITY,
+	TOGETHER_AI,
+	X_AI
+} from '@/data/models';
+
+type Provider =
+	| typeof OPEN_AI
+	| typeof ANTHROPIC
+	| typeof TOGETHER_AI
+	| typeof GOOGLE
+	| typeof GROQ
+	| typeof COHERE
+	| typeof FIREWORKS_AI
+	| typeof PERPLEXITY;
+
+/**
+ * Retrieves the provider based on the given provider string.
+ *
+ * @param providerString - The provider string.
+ * @returns The corresponding provider object.
+ * @throws Error if the provider is unknown.
+ */
+export function getProvider(providerString: string): Provider {
+	const providerMap: { [key: string]: Provider } = {
+		openai: OPEN_AI,
+		anthropic: ANTHROPIC,
+		together: TOGETHER_AI,
+		google: GOOGLE,
+		groq: GROQ,
+		cohere: COHERE,
+		fireworks: FIREWORKS_AI,
+		perplexity: PERPLEXITY,
+		ollama: OLLAMA,
+		xai: X_AI
+	};
+
+	const provider = providerMap[providerString.toLowerCase()];
+	if (!provider) {
+		throw new Error(`Unknown provider: ${providerString}`);
+	}
+	return provider;
+}
