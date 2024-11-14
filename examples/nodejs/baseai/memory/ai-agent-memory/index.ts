fa416ea71d22d2afe57a0177e97e8e01a83cd629
Saqib Ameen <saqib1@ualberta.ca>
2024-11-14 16:53:41 -0700

    📦 NEW: update config

--- examples/nodejs/baseai/memory/ai-agent-memory/index.ts
+++ examples/nodejs/baseai/memory/ai-agent-memory/index.ts
@@ -2,14 +2,12 @@ import {MemoryI} from '@baseai/core';
 
 const memoryAiAgentMemory = (): MemoryI => ({
 	name: 'ai-agent-memory',
-	description: 'My list of docs as memory for an AI agent pipe',
-	config: {
-		useGitRepo: true,
-		include: ['**/*.md', '!.baseai', '!.baseai/**'],
-		gitignore: true,
-		git: {
-			embeddedAt: 'b7d0aa946f4d9ac7322bf2d84b5e365f4b6a6ed6',
-		},
+	description: '',
+	useGit: true,
+	include: ['examples/pipe.run.ts', '!.baseai'],
+	gitignore: true,
+	git: {
+		deployedAt: 'f389dac63512f9b633bb0aef938794ef913515f0'
 	},
 });
 
