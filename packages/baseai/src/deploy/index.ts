1d77808cef43d05019943386ecd81a7ca92653bb
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-11-27 03:09:14 +0500

    👌 IMPROVE: Logs

--- packages/baseai/src/deploy/index.ts
+++ packages/baseai/src/deploy/index.ts
@@ -487,7 +487,6 @@ export async function deployMemory({
 		p.log.step(`Processing documents for memory: ${memoryNameWithoutExt}`);
 
 		if (isOldMemoryConfigFormat(memoryObject)) {
-			// spinner.stop();
 			p.note(generateUpgradeInstructions(memoryObject));
 			p.cancel(
 				'Deployment cancelled. Please update your memory config file to the new format.'
